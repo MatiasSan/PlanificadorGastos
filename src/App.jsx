de3e2d60465e731c874679074feb28fcf0430993
MatiasSan <m.coleman.villar@gmail.com>
2023-12-14 12:32:53 -0300

    Mostrando y Ocultando Modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react';
 import Header from './components/Header';
 import IconoNuevoGasto from './img/nuevo-gasto.svg';
+import Modal from './components/Modal';
 
 function App() {
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+
+  const [modal, setModal] = useState(false);
+
+  const handleNuevoGasto = () => {
+    setModal(true);
+  };
   return (
     <div>
       <Header
@@ -15,9 +22,15 @@ function App() {
       />
       {isValidPresupuesto ? (
         <div className="nuevo-gasto">
-          <img src={IconoNuevoGasto} alt="icono nuevo gasto" />
+          <img
+            src={IconoNuevoGasto}
+            alt="icono nuevo gasto"
+            onClick={handleNuevoGasto}
+          />
         </div>
       ) : null}
+
+      {modal && <Modal setModal={setModal} />}
     </div>
   );
 }
---
+++ src/components/Modal.jsx
@@ -0,0 +1,16 @@
+import CerrarBtn from '../img/cerrar.svg';
+
+const Modal = ({ setModal }) => {
+  const ocultarModal = () => {
+    setModal(false);
+  };
+  return (
+    <div className="modal">
+      <div className="cerrar-modal">
+        <img src={CerrarBtn} alt="cruz para cerrar" onClick={ocultarModal} />
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
